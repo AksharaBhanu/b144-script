fbac91007512a1607eead12ff218e78dbc0d3d08
AksharaBhanu <javagalbhanu@gmail.com>
2025-09-18 21:57:01 +0530

    selenium

--- tests/test_script1.py
+++ tests/test_script1.py
@@ -1,2 +1,7 @@
+from selenium.webdriver import Chrome
 def test_script1():
-    print('hi , GE, this is test script1')
+    print('hi , GE, this is test script1 with selenium')
+    driver=Chrome()
+    driver.get("http://www.google.com")
+    print(driver.title)
+    driver.quit()
