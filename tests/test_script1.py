894adedae131a31ea042b24ee68187e2017cd768
AksharaBhanu <javagalbhanu@gmail.com>
2025-09-19 13:33:04 +0530

    print grid url

--- tests/test_script1.py
+++ tests/test_script1.py
@@ -1,12 +1,19 @@
+import os
+
 from selenium.webdriver import Remote
 from selenium.webdriver.chrome.options import Options
 import  allure
+#pip install allure-pytest
 #pytest --alluredir=allure-results
+#allure serve allure-results
+#Environment Injector
 @allure.title('Script2-Bhanu')
 @allure.description("-Attach screenshot to allure report")
 def test_script1():
+    grid_url = os.getenv('GRID_URL', 'http://localhost:4444')
+    print(grid_url)
     with allure.step("Step1:Open Chrome Browser"):  # step name
-        driver = Remote("http://127.0.0.1:4444", options=Options())
+        driver = Remote(grid_url, options=Options())
     with allure.step("Step2:enter the URl: http://www.google.com"):
         driver.get("http://www.google.com")
 
